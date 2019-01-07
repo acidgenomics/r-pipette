50ca36909c81a3d777b1058320eb38fe1360ff39
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-07 09:15:04 -0500

    Reexport compression handlers

--- R/package.R
+++ R/package.R
@@ -31,9 +31,41 @@
 # NAMESPACE conflicts
 # import: rio, rtracklayer
 
+
+
+#' @importFrom R.utils compressFile
+#' @export
+R.utils::compressFile
+
+#' @importFrom R.utils decompressFile
+#' @export
+R.utils::decompressFile
+
+
+
+#' @importFrom R.utils bzip2
+#' @export
+R.utils::bzip2
+
+#' @importFrom R.utils bunzip2
+#' @export
+R.utils::bunzip2
+
+
+
 #' @importFrom R.utils gzip
 #' @export
 R.utils::gzip
+
+#' @importFrom R.utils gunzip
+#' @export
+R.utils::gunzip
+
+
+
+#' @importFrom utils tar
+#' @export
+utils::tar
 
 #' @importFrom utils untar
 #' @export
