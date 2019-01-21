e001b5d0f569f785624dfca87073687b467a38b7
Michael Steinbaugh <mike@steinbaugh.com>
2019-01-21 15:49:28 -0500

    Improve `sanitizeRowData()` and `sanitizeRowRanges()` consistency

--- tests/testthat/test_sanitize.R
+++ tests/testthat/test_sanitize.R
@@ -6,23 +6,28 @@ load(system.file("extdata", "rse.rda", package = "brio"))
 
 
 # sanitizeRowData ==============================================================
-
 test_that("sanitizeRowData", {
-    object <- sanitizeRowData(rowRanges(rse))
-    expect_s4_class(object, "GRanges")
-    df <- as.data.frame(object)
-    expect_true(hasRownames(df))
-    # FIXME This check is failing on AppVeyor CI.
+    object <- sanitizeRowData(rowData(rse))
+    expect_s4_class(object, "DataFrame")
+    expect_true(hasRownames(object))
     expect_identical(
-        object = lapply(df, class),
+        object = lapply(object, class),
         expected = list(
-            seqnames = "factor",
-            start = "integer",
-            end = "integer",
-            width = "integer",
-            strand = "factor",
-            # FIXME Returning as factor instead of character on BioC 3.7.
-            # Returns as character on BioC 3.8.
+            geneID = "character",
+            geneName = "factor",
+            geneBiotype = "factor",
+            broadClass = "factor"
+        )
+    )
+})
+
+test_that("sanitizeRowRanges", {
+    object <- sanitizeRowRanges(rowRanges(rse))
+    expect_s4_class(object, "GRanges")
+    expect_true(hasNames(object))
+    expect_identical(
+        object = lapply(S4Vectors::mcols(object), class),
+        expected = list(
             geneID = "character",
             geneName = "factor",
             geneBiotype = "factor",
