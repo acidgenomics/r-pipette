8080e9179b5626e46fcfe651de12dc28a0615289
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-17 13:22:47 -0500

    Remove dependency on tools package

--- R/basenameSansExt.R
+++ R/basenameSansExt.R
@@ -1,13 +1,20 @@
 #' Get the base name without the file extension
-#' 
+#'
+#' @details
+#' This function is vectorized and supports multiple file paths.
+#'
 #' @export
 #' @inherit base::basename
-#' 
-#' @seealso [`basename()`][base::basename].
-#' 
+#'
+#' @seealso
+#' - [`basename()`][base::basename].
+#' - `tools::file_path_sans_ext()`.
+#'
 #' @examples
-#' basenameSansExt("dir/file.txt")
-#' basenameSansExt("dir/archive.tar.gz")
+#' basenameSansExt(c("dir/file.txt", "dir/archive.tar.gz"))
 basenameSansExt <- function(path) {
-    file_path_sans_ext(basename(path), compression = TRUE)
+    x <- basename(path = path)
+    x <- sub(pattern = compressExtPattern, replacement = "", x = x)
+    x <- sub(pattern = "([^.]+)\\.[[:alnum:]]+$", replacement = "\\1", x = x)
+    x
 }
