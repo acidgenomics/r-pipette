6221677c91bcab994bddf6551cda41b70484fe9d
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-05 10:13:36 -0500

    Slot package versions into metadata

--- R/importRDA.R
+++ R/importRDA.R
@@ -1,12 +1,11 @@
 importRDA <- function(file, ...) {
     file <- localOrRemoteFile(file)
-    message(paste(
-        "Importing", basename(file), "using base::load()."
-    ))
+    message(paste("Importing", basename(file), "using base::load()."))
     safe <- new.env()
     object <- load(file, envir = safe, ...)
     if (length(safe) != 1L) {
         stop("File does not contain a single object.")
     }
-    get(object, envir = safe, inherits = FALSE)
+    object <- get(object, envir = safe, inherits = FALSE)
+    object
 }
