26382655f4f17b8193ce3e3defd352c1d044e55a
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-05 11:32:02 -0500

    Improve data providence by stashing metadata

--- R/importYAML.R
+++ R/importYAML.R
@@ -3,6 +3,6 @@ importYAML <- function(file, ...) {
     message(paste("Importing", basename(file), "using yaml::yaml.load_file()."))
     requireNamespace("yaml", quietly = TRUE)
     object <- yaml::yaml.load_file(input = file, ...)
-    object <- .slotVersion(object, pkg = "yaml")
+    object <- .slotMetadata(object, pkg = "yaml", fun = "yaml.load_file")
     object
 }
