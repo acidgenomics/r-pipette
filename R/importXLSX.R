db25bb18f591ef9d43d6dc017f09bfc0dafed46e
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-08 09:05:32 -0500

    Improve argument passthrough for `importXLSX()`

--- R/importXLSX.R
+++ R/importXLSX.R
@@ -1,10 +1,25 @@
-importXLSX <- function(file, ...) {
+importXLSX <- function(file, sheet = 1L) {
     file <- localOrRemoteFile(file)
     message(paste("Importing", basename(file), "using readxl::read_excel()."))
     requireNamespace("readxl", quietly = TRUE)
-    object <- readxl::read_excel(path = file, na = naStrings, ...)
-    # Coerce tbl_df to data.frame.
-    object <- as.data.frame(object)
+    object <- readxl::read_excel(
+        path = file,
+        sheet = sheet,
+        col_names = TRUE,
+        na = naStrings,
+        # Keep quiet.
+        progress = FALSE,
+        # Don't attempt name repair.
+        # Refer to `tibble()` documentation for details.
+        .name_repair = "minimal"
+    )
+    # Always return as data.frame instead of tibble at this step.
+    # Refer to S3 method for class `matrix` for supported params.
+    object <- as.data.frame(
+        x = object,
+        make.names = FALSE,
+        stringsAsFactors = FALSE
+    )
     object <- .slotMetadata(object, pkg = "readxl", fun = "read_excel")
     object
 }
