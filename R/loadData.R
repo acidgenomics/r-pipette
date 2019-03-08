4da38096e1609cb089cdab1c103f38fa6cc2b94b
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-08 12:44:58 -0500

    Resave examples

--- R/loadData.R
+++ R/loadData.R
@@ -1,18 +1,18 @@
 #' Load data
 #'
 #' Load R data files from a directory using symbols rather than complete file
-#' paths. Supports "`.rds`", "`.rda`", and "`.RData`" file extensions.
+#' paths. Supports `RDS`, `RDA`, and `RDATA` file extensions.
 #'
-#' `loadData` is opinionated about the format of R data files it will accept.
-#' `save` allows for the saving of multiple objects into a single R data file.
-#' This can later result in unexpected accidental replacement of an existing
-#' object in the current environment. Since an R data file internally stores the
-#' name of an object, if the file is later renamed the object name will no
-#' longer match.
+#' [loadData()] is opinionated about the format of R data files it will accept.
+#' [`save()`][base::save] allows for the saving of multiple objects into a
+#' single R data file. This can later result in unexpected accidental
+#' replacement of an existing object in the current environment. Since an R data
+#' file internally stores the name of an object, if the file is later renamed
+#' the object name will no longer match.
 #'
-#' To avoid any accidental replacements, `loadData` will only load R data
+#' To avoid any accidental replacements, [loadData()] will only load R data
 #' files that contain a single object, and the internal object name must match
-#' the file name exactly. Additionally, `loadData` will intentionally error if
+#' the file name exactly. Additionally, [loadData()] will intentionally error if
 #' an object with the same name already exists in the destination `environment`.
 #'
 #' @note This function is desired for interactive use and interprets object
@@ -42,11 +42,17 @@
 #' ## Note that this method uses non-standard evaluation.
 #' loadData(rse, sce, dir = dir)
 #'
+#' ## Clean up.
+#' rm(rse, sce)
+#'
 #' ## List mode ====
 #' ## Note that this method uses standard evaluation.
 #' ## Use this approach inside of functions.
 #' list <- c("rse", "sce")
-#' loadData(list = list)
+#' loadData(list = list, dir = dir)
+#'
+#' ## Clean up.
+#' rm(rse, sce)
 loadData <- function(
     ...,
     list = NULL,
