b980ef49ba091aa8c71084e64ff832882e8be504
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-13 10:02:07 -0400

    Split out XLS file loading to a separate importer

---
+++ R/importXLS.R
@@ -0,0 +1,19 @@
+# readxl does support XLS format also but it's buggy for many files.
+# Make some minimal repex examples and file issue on GitHub.
+# See also:
+# - https://github.com/tidyverse/readxl/issues/466
+# - https://github.com/tidyverse/readxl/issues/472
+# In the meantime, load using gdata, which is slow but does work.
+importXLS <- function(file, sheet = 1L) {
+    file <- localOrRemoteFile(file)
+    message(paste("Importing", basename(file), "using gdata::read.xls()."))
+    requireNamespace("gdata", quietly = TRUE)
+    object <- gdata::read.xls(
+        xls = file,
+        sheet = sheet,
+        verbose = FALSE,
+        na.strings = naStrings
+    )
+    object <- .slotMetadata(object, pkg = "gdata", fun = "read.xls")
+    object
+}
