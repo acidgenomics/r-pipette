f3ab894a236fa23c300a9d24c2c0cabbc6bd0368
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 11:35:45 -0400

    `export()`: Improve sparseMatrix coverage

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -23,6 +23,45 @@ test_that("Check for error on both `file` and `format` argument.", {
 
 
 
+context("export : sparseMatrix")
+
+test_that("`file` argument", {
+    x <- export(sparse, file = "sparse.mtx")
+    expect_identical(
+        x,
+        c(
+            matrix = realpath("sparse.mtx"),
+            barcodes = realpath("sparse.mtx.colnames"),
+            genes = realpath("sparse.mtx.rownames")
+        )
+    )
+    expect_true(all(file.exists(x)))
+    file.remove(x)
+})
+
+test_that("`format` argument, using gzip compression", {
+    x <- export(sparse, format = "mtx.gz")
+    expect_identical(
+        x,
+        c(
+            matrix = realpath("sparse.mtx.gz"),
+            barcodes = realpath("sparse.mtx.gz.colnames"),
+            genes = realpath("sparse.mtx.gz.rownames")
+        )
+    )
+    expect_true(all(file.exists(x)))
+    file.remove(x)
+})
+
+test_that("Check for error on both `file` and `format` argument.", {
+    expect_error(
+        object = export(sparse, file = "sparse", format = "mtx"),
+        regexp = "Specify `file` or `format` but not both."
+    )
+})
+
+
+
 context("export : SummarizedExperiment")
 
 test_that("`dir` argument, no `name`", {
