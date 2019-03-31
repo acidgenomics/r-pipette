1f2a23c1d5c53306c7891c7c568178c0435cf53e
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-31 14:58:43 -0400

    Pass lintr checks

--- tests/testthat/test-sanitizePercent.R
+++ tests/testthat/test-sanitizePercent.R
@@ -4,6 +4,6 @@ test_that("sanitizePercent", {
     object <- c("100%", "10.0%", "1%", "0.1%", "0.01%")
     expect_identical(
         sanitizePercent(object),
-        c(1e+00, 1e-01, 1e-02, 1e-03, 1e-04)
+        c(1e+00, 1e-01, 1e-02, 1e-03, 1e-04)  # nolint
     )
 })
