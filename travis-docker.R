a20097dfbca748227fd36e09ad73432b8fcfa2df
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-04 14:38:29 -0400

    Improve CI config

--- travis-docker.R
+++ travis-docker.R
@@ -1,3 +1,4 @@
-rcmdcheck::rcmdcheck(args = "--no-manual")
-BiocCheck::BiocCheck()
+Sys.setenv(TZ = "America/New_York")
+rcmdcheck::rcmdcheck(args = "--no-build-vignettes --no-manual --timings")
+BiocCheck::BiocCheck(`quit-with-status` = FALSE)
 lintr::lint_package()
