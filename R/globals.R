e751a3ef5b757633f6cd192c07784cb112d8e5c5
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-06 15:52:20 -0400

    Update formalsList

--- R/globals.R
+++ R/globals.R
@@ -42,22 +42,51 @@ compressExtPattern <- paste0(compressExtPattern, "$")
 
 formalsList <- list(
     data.frame = quote(
-        getOption("acid.data.frame", default = "data.frame")
+        getOption(
+            "acid.data.frame",
+            default = c(
+                "data.frame",
+                "DataFrame",
+                "tbl_df",
+                "data.table"
+            )
+        )
+    ),
+    export.compress = quote(
+        getOption("acid.export.compress", default = FALSE)
+    ),
+    export.dir = quote(
+        getOption("acid.export.dir", default = ".")
+    ),
+    export.ext = quote(
+        getOption(
+            "acid.export.ext",
+            default = c("csv", "csv.gz", "tsv", "tsv.gz")
+        )
+    ),
+    export.overwrite = quote(
+        getOption("acid.export.overwrite", default = TRUE)
+    ),
+    export.sparse.ext = quote(
+        getOption(
+            "acid.export.sparse.ext",
+            default = c("mtx", "mtx.gz")
+        )
     ),
     load.dir = quote(
         getOption("acid.load.dir", default = ".")
+    ),
+    save.compress = quote(
+        getOption("acid.save.compress", default = TRUE)
     ),
     save.dir = quote(
         getOption("acid.save.dir", default = ".")
     ),
     save.ext = quote(
-        getOption("acid.save.ext", default = "rds")
+        getOption("acid.save.ext", default = c("rds", "rda"))
     ),
     save.overwrite = quote(
         getOption("acid.save.overwrite", default = TRUE)
-    ),
-    save.compress = quote(
-        getOption("acid.save.compress", default = TRUE)
     )
 )
 
