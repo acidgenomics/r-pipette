55fbad57272d5999ab4273a42c61e13626eeb8b7
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-16 14:09:06 -0400

    Improve factorize unit test

--- tests/testthat/test-factorize.R
+++ tests/testthat/test-factorize.R
@@ -1,14 +1,9 @@
 context("factorize")
 
-test_that("factorize", {
-    df <- DataFrame(
-        a = letters[seq_len(5L)],
-        b = seq_len(5L)
-    )
-    x <- factorize(df)
-    expect_s4_class(x, "DataFrame")
-    expect_identical(
-        vapply(x, class, character(1L)),
-        c(a = "factor", b = "factor")
+test_that("DataFrame", {
+    object <- factorize(df)
+    expect_s4_class(object, "DataFrame")
+    expect_true(
+        all(vapply(X = object, FUN = is.factor, FUN.VALUE = logical(1L)))
     )
 })
