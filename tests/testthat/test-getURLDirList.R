c06aecff735cc07b48f4e6b3a441d47bb965a31a
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-18 16:11:29 -0500

    Update unit test

--- tests/testthat/test-getURLDirList.R
+++ tests/testthat/test-getURLDirList.R
@@ -4,8 +4,8 @@ skip_if_not(hasInternet())
 skip_on_appveyor()
 skip_on_docker()
 
-test_that("Ensembl FTP", {
-    url <- "ftp://ftp.ensembl.org/pub/"
-    x <- getURLDirList(url, pattern = "^release-")
+test_that("NCBI FTP", {
+    url <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/Homo_sapiens/current"
+    x <- getURLDirList(url, pattern = "^GCF_")
     expect_is(x, "character")
 })
