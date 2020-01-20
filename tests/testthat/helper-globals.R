71b114ae251d8900c99a186e7b56a38acaddc11e
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-19 22:20:42 -0500

    Migrate unit tests from transformer

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,22 +1,39 @@
 data(
     DFrame,
     GRanges,
+    IRanges,
     RangedSummarizedExperiment,
     SingleCellExperiment,
     SingleCellExperiment_Seurat,
     data.table,
+    sparseMatrix,
     tbl_df,
     package = "acidtest",
     envir = environment()
 )
 
+stopifnot(
+    is(DFrame, "DataFrame"),
+    is(GRanges, "GRanges"),
+    is(IRanges, "IRanges"),
+    is(RangedSummarizedExperiment, "RangedSummarizedExperiment"),
+    is(data.table, "data.table"),
+    is(sparseMatrix, "sparseMatrix"),
+    is(tbl_df, "tbl_df")
+)
+
 df <- DFrame
 dt <- data.table
 gr <- GRanges
+ir <- IRanges
 rse <- RangedSummarizedExperiment
 sce <- SingleCellExperiment
 sce_seurat <- SingleCellExperiment_Seurat  # nolint
+sparse <- sparseMatrix
 tbl <- tbl_df
+
+data(mtcars, package = "datasets", envir = environment())
+mtcars <- as(mtcars, "DataFrame")
 
 ## nolint start
 DataFrame <- S4Vectors::DataFrame
@@ -24,12 +41,17 @@ assay <- SummarizedExperiment::assay
 assayNames <- SummarizedExperiment::assayNames
 assays <- SummarizedExperiment::assays
 `assays<-` <- SummarizedExperiment::`assays<-`
+colData <- SummarizedExperiment::colData
+data.table <- data.table::data.table
 hasInternet <- goalie::hasInternet
+isSubset <- goalie::isSubset
 mcols <- S4Vectors::mcols
 metadata <- S4Vectors::metadata
+rowData <- SummarizedExperiment::rowData
 rowRanges <- SummarizedExperiment::rowRanges
 seqnames <- GenomicRanges::seqnames
 skip_on_docker <- goalie::skip_on_docker
+tibble <- tibble::tibble
 ## nolint end
 
 mat <- assay(rse)
