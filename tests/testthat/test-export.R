8f2c25c5c796ec0b91f18afcf334b7abc528cf98
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-06 12:10:24 -0400

    Improve code coverage

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -62,20 +62,14 @@ test_that("'ext' argument", {
     }
 })
 
-test_that("readr mode (experimental)", {
-    options("acid.export.engine" = "readr")
-    file <- export(object = mat, ext = "csv")
-    expect_true(file.exists(file))
-    expect_identical(basename(file), "mat.csv")
-    unlink(file)
-})
-
-test_that("vroom mode (experimental)", {
-    options("acid.export.engine" = "vroom")
-    file <- export(object = mat, ext = "csv")
-    expect_true(file.exists(file))
-    expect_identical(basename(file), "mat.csv")
-    unlink(file)
+test_that("Specify the internal import engine (experimental)", {
+    for (engijne in c("data.table", "readr", "vroom")) {
+        options("acid.export.engine" = engine)
+        file <- export(object = mat, ext = "csv")
+        expect_true(file.exists(file))
+        expect_identical(basename(file), "mat.csv")
+        unlink(file)
+    }
 })
 
 test_that("Invalid input", {
