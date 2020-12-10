dedd46122db093ce71175d3f9e6695d2f11f947c
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-10 11:42:10 -0500

    Update reexports from AcidBase

--- R/reexports.R
+++ R/reexports.R
@@ -1,3 +1,15 @@
+#' @importFrom AcidBase compress
+#' @export
+AcidBase::compress
+
+#' @importFrom AcidBase decompress
+#' @export
+AcidBase::decompress
+
+#' @importFrom AcidBase download
+#' @export
+AcidBase::download
+
 #' @importFrom AcidBase pasteURL
 #' @export
 AcidBase::pasteURL
