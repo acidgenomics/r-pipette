d7a173246069d2e66b4512c39dd151d21169190c
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-18 14:09:16 -0500

    Simplify internal bcbio counts import to use default TSV import method

--- R/import.R
+++ R/import.R
@@ -1008,45 +1008,43 @@ formals(import)[c("makeNames", "metadata", "quiet")] <-
 
 
 ## bcbio =======================================================================
-## Internal importer for a bcbio count matrix file (`.counts`).
-## These files contain an `"id"` column that we need to coerce to row names.
-## Updated 2020-12-15.
+#' Import bcbio featureCounts file
+#'
+#' @details
+#' Internal importer for a bcbio count matrix file (`.counts`).
+#' These files contain an `"id"` column that we need to coerce to row names.
+#'
+#' @note Updated 2020-12-18.
+#' @noRd
 .importBcbioCounts <- function(file, metadata, quiet) {
-    requireNamespaces("data.table")
     assert(
         isFlag(metadata),
         isFlag(quiet)
     )
-    if (!isTRUE(quiet)) {
-        where <- ifelse(
-            test = isAURL(file),
-            yes = dirname(file),
-            no = realpath(dirname(file))
+    object <- import(
+        file = file,
+        format = "tsv",
+        metadata = metadata,
+        quiet = quiet
+    )
+    if (isTRUE(metadata)) {
+        m <- metadata2(object, which = "import")
+        assert(
+            is.list(m),
+            hasLength(m)
         )
-        cli_alert(sprintf(
-            "Importing {.file %s} at {.path %s} using {.pkg %s}::{.fun %s}.",
-            basename(file), where,
-            "data.table", "fread"
-        ))
     }
-    tmpfile <- localOrRemoteFile(file = file, quiet = quiet)
-    object <- data.table::fread(file = tmpfile, na.strings = naStrings)
     assert(
+        is.data.frame(object),
         isSubset("id", colnames(object)),
         hasNoDuplicates(object[["id"]])
     )
-    object <- as.data.frame(object)
     rownames(object) <- object[["id"]]
     object[["id"]] <- NULL
     object <- as.matrix(object)
     mode(object) <- "integer"
     if (isTRUE(metadata)) {
-        object <- .slotImportMetadata(
-            object = object,
-            file = file,
-            pkg = "data.table",
-            fun = "fread"
-        )
+        metadata2(object, which = "import") <- m
     }
     object
 }
