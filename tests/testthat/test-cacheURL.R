42fe6d96a1a4b91f8d0e42ce6f94e59079a27b33
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-29 15:32:53 -0500

    Files with the same basename aren't caching correctly

--- tests/testthat/test-cacheURL.R
+++ tests/testthat/test-cacheURL.R
@@ -10,3 +10,43 @@ test_that("URL into BiocFileCache", {
     expect_is(file, "character")
     expect_match(object = file, regexp = basename(url))
 })
+
+test_that("URLs with the same base name", {
+    ## SHA256: 9d77df788bc30aa86d6aa8d4b8d5bbe9d9c2a0ae6c0a8eda600524685aa11875
+    url1 <- pasteURL(
+        "ftp.ensembl.org",
+        "pub",
+        "release-87",
+        "fasta",
+        "homo_sapiens",
+        "cdna",
+        "Homo_sapiens.GRCh38.cdna.all.fa.gz",
+        protocol = "ftp"
+    )
+    lines <- import(file = url1, format = "lines")
+    expect_identical(
+        object = length(lines),
+        expected = 5063317L
+    )
+    ## SHA256: 0adbf00421ad63b7139481cefe03d9e3d226945696b17e48a4396c5d663ee7b0
+    url2 <- pasteURL(
+        "ftp.ensembl.org",
+        "pub",
+        "release-102",
+        "fasta",
+        "homo_sapiens",
+        "cdna",
+        "Homo_sapiens.GRCh38.cdna.all.fa.gz",
+        protocol = "ftp"
+    )
+    lines <- import(file = url2, format = "lines")
+    expect_identical(
+        object = length(lines),
+        expected = 5883368L
+    )
+    cacheFile1 <- cacheURL(url1)
+    cacheFile2 <- cacheURL(url2)
+    expect_true(file.exists(cacheFile1))
+    expect_true(file.exists(cacheFile2))
+    expect_false(identical(cacheFile1, cacheFile2))
+})
