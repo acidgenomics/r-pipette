d34b57ef3c51dd652152907e133dbfec61b38adc
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-02 15:46:31 -0500

    Simplify unit tests

--- tests/testthat/test-encode-decode.R
+++ tests/testthat/test-encode-decode.R
@@ -13,12 +13,3 @@ test_that("GRanges", {
     y <- decode(x)
     expect_type(mcols(y)[[1L]], "character")
 })
-
-test_that("SummarizedExperiment", {
-    x <- encode(rse)
-    expect_s4_class(rowData(x)[[1L]], "Rle")
-    expect_s4_class(colData(x)[[1L]], "Rle")
-    y <- decode(rse)
-    expect_s3_class(rowData(y)[[1L]], "factor")
-    expect_s3_class(colData(y)[[1L]], "factor")
-})
