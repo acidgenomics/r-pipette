7e72ba050edd08728576fdefaa698c5adc185cec
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-05 16:49:55 -0500

    Improve ordering of coercion methods

--- R/coerce-methods.R
+++ R/coerce-methods.R
@@ -1,3 +1,12 @@
+## This is needed to properly declare S4 `as()` coercion methods.
+#' @name coerce
+#' @export
+#' @importFrom methods coerce
+#' @exportMethod coerce
+NULL
+
+
+
 #' Coercion methods
 #'
 #' @name coerce
@@ -13,6 +22,40 @@
 #' Don't define `as()` coercion method for `list` here. It will create issues
 #' with `data.frame` coercion. Use [as.DataFrame()] instead to coerce a `list`
 #' to `DataFrame`.
+#'
+#' Wrapping the columns in an `I()` works when passing to `DataFrame()`.
+#' See also `as_tibble()` for easy list to data frame coercion.
+#'
+#' `as()` method definition causes issues with `data.frame` to `DataFrame`
+#' coercion when defined, because `data.frame` inherits from list.
+#'
+#' @section `data.frame` coercion:
+#'
+#' ## To `IRanges`
+#'
+#' Default coercion of `IPosRanges` (i.e. `IRanges`) to `data.frame` currently
+#' strips metadata in `mcols()`. However, GenomicRanges preserves this
+#' information, so we're adding a tweaked coercion method here to improve
+#' consistency.
+#'
+#' Relevant methods:
+#'
+#' ```r
+#' getMethod(
+#'     f = "as.data.frame",
+#'     signature = "GenomicRanges",
+#'     where = asNamespace("GenomicRanges")
+#' )
+#' ## IRanges inherits from `IPosRanges`.
+#' getMethod(
+#'     f = "as.data.frame",
+#'     signature = "IPosRanges",
+#'     where = asNamespace("IRanges")
+#' )
+#' ```
+#'
+#' See also:
+#' - https://github.com/Bioconductor/IRanges/issues/8
 #'
 #' @section `data.table` coercion:
 #'
@@ -43,6 +86,10 @@
 #' to ensure that row names are not dropped by default, which is a poor default
 #' for bioinformatics. This is accomplished by setting `rownames = "rowname"` by
 #' default instead of `rownames = NULL`.
+#'
+#' Note that we're matching `as_tibble()` convention here, using `rowname` as
+#' column for row names assignment. We also using similar internal assert checks
+#' here, allowing atomic and/or list columns only.
 #'
 #' ## S3: `as_tibble()`
 #'
@@ -140,273 +187,7 @@ NULL
 
 
 
-## This is needed to properly declare S4 `as()` coercion methods.
-#' @name coerce
-#' @export
-#' @importFrom methods coerce
-#' @exportMethod coerce
-NULL
-
-
-
-## Default coercion of IPosRanges (i.e. IRanges) to data.frame currently
-## strips metadata in `mcols()`. However, GenomicRanges preserves this
-## information, so we're adding a tweaked coercion method here to improve
-## consistency.
-##
-## Relevant methods:
-## > getMethod(
-## >     f = "as.data.frame",
-## >     signature = "GenomicRanges",
-## >     where = asNamespace("GenomicRanges")
-## > )
-## IRanges inherits from `IPosRanges`.
-## > getMethod(
-## >     f = "as.data.frame",
-## >     signature = "IPosRanges",
-## >     where = asNamespace("IRanges")
-## > )
-##
-## See also:
-## - https://github.com/Bioconductor/IRanges/issues/8
-##
-## Updated 2020-01-19.
-`as.data.frame,IRanges` <-  # nolint
-    function(
-        x,
-        row.names = NULL,
-        optional = FALSE,
-        ...
-    ) {
-        if (missing(row.names)) {
-            row.names <- names(x)
-        }
-        if (!is.null(names(x))) {
-            names(x) <- NULL
-        }
-        args <- list(
-            start = start(x),
-            end = end(x),
-            width = width(x),
-            row.names = row.names,
-            check.rows = TRUE,
-            check.names = FALSE,
-            stringsAsFactors = FALSE
-        )
-        mcols <- mcols(x, use.names = FALSE)
-        if (!is.null(mcols)) {
-            args[["mcols"]] <- as.data.frame(mcols)
-        }
-        do.call(what = data.frame, args = args)
-    }
-
-
-
-#' @rdname coerce
-#' @export
-setMethod(
-    f = "as.data.frame",
-    signature = signature("IRanges"),
-    definition = `as.data.frame,IRanges`
-)
-
-
-
-## Updated 2019-07-20.
-`as.data.frame,Matrix` <-  # nolint
-    function(x, ...) {
-        as.data.frame(as.matrix(x), ...)
-    }
-
-
-
-#' @rdname coerce
-#' @export
-setMethod(
-    f = "as.data.frame",
-    signature = signature("Matrix"),
-    definition = `as.data.frame,Matrix`
-)
-
-
-
-`coerce,ANY,data.frame` <-  # nolint
-    function(from) {
-        as.data.frame(from)
-    }
-
-
-
-## Updated 2019-07-19.
-`coerce,IRanges,data.frame` <-  # nolint
-    `coerce,ANY,data.frame`
-
-
-
-#' @rdname coerce
-#' @name coerce,IRanges,data.frame-method
-setAs(
-    from = "IRanges",
-    to = "data.frame",
-    def = `coerce,IRanges,data.frame`
-)
-
-
-
-## Updated 2019-07-20.
-`coerce,Matrix,data.frame` <-  # nolint
-    `coerce,ANY,data.frame`
-
-
-
-#' @rdname coerce
-#' @name coerce,Matrix,data.frame-method
-setAs(
-    from = "Matrix",
-    to = "data.frame",
-    def = `coerce,Matrix,data.frame`
-)
-
-
-
-
-
-
-
-#' @rdname coerce
-#' @export
-## Updated 2019-07-19.
-as_tibble.DataFrame <-  # nolint
-    function(x, ..., rownames) {
-        x <- `.coerce,DataFrame,data.frame`(x)
-        if (!hasRownames(x)) {
-            rownames <- NULL
-        }
-        as_tibble(x = x, ..., rownames = rownames)
-    }
-
-
-
-
-
-#' @rdname coerce
-#' @export
-## Updated 2020-01-19.
-as_tibble.IRanges <-  # nolint
-    function(x, ..., rownames) {
-        x <- as(x, "data.frame")
-        if (!hasRownames(x)) {
-            rownames <- NULL
-        }
-        as_tibble(x = x, ..., rownames = rownames)
-    }
-
-
-
-.tbl_rownames <-  # nolint
-    quote(pkgconfig::get_config("tibble::rownames", "rowname"))
-formals(as_tibble.DataFrame)[["rownames"]] <- .tbl_rownames
-formals(as_tibble.IRanges)[["rownames"]] <- .tbl_rownames
-rm(.tbl_rownames)
-
-
-
-#' @rdname coerce
-#' @export
-## Updated 2020-01-19.
-as_tibble.GenomicRanges <- as_tibble.IRanges  # nolint
-
-
-
-## Updated 2019-07-19.
-`coerce,ANY,tbl_df` <-  # nolint
-    function(from) {
-        as_tibble(from)
-    }
-
-
-
-## Updated 2019-07-19.
-`coerce,data.frame,tbl_df` <-  # nolint
-    `coerce,ANY,tbl_df`
-
-
-
-#' @rdname coerce
-#' @name coerce,data.frame,tbl_df-method
-setAs(
-    from = "data.frame",
-    to = "tbl_df",
-    def = `coerce,data.frame,tbl_df`
-)
-
-
-
-## Updated 2019-07-19.
-`coerce,DataFrame,tbl_df` <-  # nolint
-    `coerce,ANY,tbl_df`
-
-
-
-#' @rdname coerce
-#' @name coerce,DataFrame,tbl_df-method
-setAs(
-    from = "DataFrame",
-    to = "tbl_df",
-    def = `coerce,DataFrame,tbl_df`
-)
-
-
-
-## Updated 2019-07-20.
-`coerce,GenomicRanges,tbl_df` <-  # nolint
-    `coerce,ANY,tbl_df`
-
-
-
-#' @rdname coerce
-#' @name coerce,GenomicRanges,tbl_df-method
-setAs(
-    from = "GenomicRanges",
-    to = "tbl_df",
-    def = `coerce,GenomicRanges,tbl_df`
-)
-
-
-
-## Updated 2020-01-19.
-`coerce,IRanges,tbl_df` <-  # nolint
-    `coerce,ANY,tbl_df`
-
-
-
-#' @rdname coerce
-#' @name coerce,IRanges,tbl_df-method
-setAs(
-    from = "IRanges",
-    to = "tbl_df",
-    def = `coerce,IRanges,tbl_df`
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Wrapping the columns in an `I()` works when passing to `DataFrame()`.
-## See also `as_tibble()` for easy list to data frame coercion.
-##
-## `as()` method definition causes issues with `data.frame` to `DataFrame`
-## coercion when defined, because data.frame inherits from list.
-##
+## To DataFrame ================================================================
 ## Updated 2010-01-08.
 `as.DataFrame,list` <-  # nolint
     function(x, row.names = NULL) {
@@ -416,33 +197,9 @@ setAs(
         )
     }
 
-
-
-#' @rdname coerce
-#' @export
-setMethod(
-    f = "as.DataFrame",
-    signature = signature("list"),
-    definition = `as.DataFrame,list`
-)
-
-
-
 ## Updated 2021-02-03.
 `as.DataFrame,List` <-  # nolint
     `as.DataFrame,list`
-
-
-
-#' @rdname coerce
-#' @export
-setMethod(
-    f = "as.DataFrame",
-    signature = signature("List"),
-    definition = `as.DataFrame,List`
-)
-
-
 
 ## Updated 2021-01-15.
 `coerce,ANY,DataFrame` <-  # nolint
@@ -460,39 +217,13 @@ setMethod(
         to
     }
 
-
-
 ## Updated 2019-07-12.
 `coerce,Matrix,DataFrame` <-  # nolint
     `coerce,ANY,DataFrame`
 
-
-
-#' @rdname coerce
-#' @name coerce,Matrix,DataFrame-method
-setAs(
-    from = "Matrix",
-    to = "DataFrame",
-    def = `coerce,Matrix,DataFrame`
-)
-
-
-
 ## Updated 2019-07-12.
 `coerce,data.table,DataFrame` <-  # nolint
     `coerce,ANY,DataFrame`
-
-
-
-#' @rdname coerce
-#' @name coerce,data.table,DataFrame-method
-setAs(
-    from = "data.table",
-    to = "DataFrame",
-    def = `coerce,data.table,DataFrame`
-)
-
-
 
 ## Updated 2019-07-12.
 `coerce,tbl_df,DataFrame` <-  # nolint
@@ -500,31 +231,120 @@ setAs(
 
 
 
+## To data.frame ===============================================================
+## Updated 2019-07-20.
+`as.data.frame,Matrix` <-  # nolint
+    function(x, ...) {
+        as.data.frame(as.matrix(x), ...)
+    }
+
+## Updated 2021-02-05.
+`as.data.frame,IRanges` <-  # nolint
+    function(
+        x,
+        row.names = NULL,
+        optional = FALSE,
+        ...
+    ) {
+        if (missing(row.names)) {
+            row.names <- names(x)
+        }
+        if (!is.null(names(x))) {
+            names(x) <- NULL
+        }
+        args <- list(
+            "start" = start(x),
+            "end" = end(x),
+            "width" = width(x),
+            "row.names" = row.names,
+            "check.rows" = TRUE,
+            "check.names" = FALSE,
+            "stringsAsFactors" = FALSE
+        )
+        mcols <- mcols(x, use.names = FALSE)
+        if (!is.null(mcols)) {
+            args[["mcols"]] <- as.data.frame(mcols)
+        }
+        do.call(what = data.frame, args = args)
+    }
+
+## Updated 2019-07-20.
+`coerce,ANY,data.frame` <-  # nolint
+    function(from) {
+        as.data.frame(from)
+    }
+
+## Updated 2019-07-19.
+`coerce,IRanges,data.frame` <-  # nolint
+    `coerce,ANY,data.frame`
+
+## Updated 2019-07-20.
+`coerce,Matrix,data.frame` <-  # nolint
+    `coerce,ANY,data.frame`
+
+
+
+## To tibble ===================================================================
 #' @rdname coerce
-#' @name coerce,tbl_df,DataFrame-method
-setAs(
-    from = "tbl_df",
-    to = "DataFrame",
-    def = `coerce,tbl_df,DataFrame`
-)
+#' @export
+## Updated 2019-07-19.
+as_tibble.DataFrame <-  # nolint
+    function(x, ..., rownames) {
+        x <- `.coerce,DataFrame,data.frame`(x)
+        if (!hasRownames(x)) {
+            rownames <- NULL
+        }
+        as_tibble(x = x, ..., rownames = rownames)
+    }
+
+#' @rdname coerce
+#' @export
+## Updated 2020-01-19.
+as_tibble.IRanges <-  # nolint
+    function(x, ..., rownames) {
+        x <- as(x, "data.frame")
+        if (!hasRownames(x)) {
+            rownames <- NULL
+        }
+        as_tibble(x = x, ..., rownames = rownames)
+    }
+
+#' @rdname coerce
+#' @export
+## Updated 2020-01-19.
+as_tibble.GenomicRanges <- as_tibble.IRanges  # nolint
+
+## Updated 2019-07-19.
+`coerce,ANY,tbl_df` <-  # nolint
+    function(from) {
+        as_tibble(from)
+    }
+
+## Updated 2019-07-19.
+`coerce,data.frame,tbl_df` <-  # nolint
+    `coerce,ANY,tbl_df`
+
+## Updated 2019-07-19.
+`coerce,DataFrame,tbl_df` <-  # nolint
+    `coerce,ANY,tbl_df`
+
+## Updated 2019-07-20.
+`coerce,GenomicRanges,tbl_df` <-  # nolint
+    `coerce,ANY,tbl_df`
+
+## Updated 2020-01-19.
+`coerce,IRanges,tbl_df` <-  # nolint
+    `coerce,ANY,tbl_df`
+
+.tbl_rownames <-  # nolint
+    quote(pkgconfig::get_config("tibble::rownames", "rowname"))
+formals(as_tibble.DataFrame)[["rownames"]] <- .tbl_rownames
+formals(as_tibble.IRanges)[["rownames"]] <- .tbl_rownames
+rm(.tbl_rownames)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-## Note that we're matching `as_tibble()` convention here, using "rowname" as
-## column for row names assignment. We also using similar internal assert checks
-## here, allowing atomic and/or list columns only.
-
+## To data.table ===============================================================
 #' @rdname coerce
 #' @export
 ## Updated 2019-07-19.
@@ -536,11 +356,6 @@ as.data.table.DataFrame <-  # nolint
         }
         as.data.table(x = x, keep.rownames = keep.rownames, ...)
     }
-
-
-
-## The default handling from data.frame isn't clean, so add this.
-## Default method will warn: `Arguments in '...' ignored`.
 
 #' @rdname coerce
 #' @export
@@ -554,15 +369,11 @@ as.data.table.IRanges <-  # nolint
         as.data.table(x = x, keep.rownames = keep.rownames, ...)
     }
 
-
-
 #' @rdname coerce
 #' @export
 ## Updated 2020-01-19.
 as.data.table.GenomicRanges <-  # nolint
     as.data.table.IRanges
-
-
 
 ## Updated 2019-07-19.
 `coerce,ANY,data.table` <-  # nolint
@@ -570,28 +381,61 @@ as.data.table.GenomicRanges <-  # nolint
         as.data.table(from)
     }
 
-
 ## Updated 2019-07-19.
 `coerce,data.frame,data.table` <-  # nolint
     `coerce,ANY,data.table`
-
-
-
-#' @rdname coerce
-#' @name coerce,data.frame,data.table-method
-setAs(
-    from = "data.frame",
-    to = "data.table",
-    def = `coerce,data.frame,data.table`
-)
-
-
 
 ## Updated 2019-07-19.
 `coerce,DataFrame,data.table` <-  # nolint
     `coerce,ANY,data.table`
 
+## Updated 2020-01-19.
+`coerce,IRanges,data.table` <-  # nolint
+    `coerce,ANY,data.table`
 
+## Updated 2019-07-20.
+`coerce,GenomicRanges,data.table` <-  # nolint
+    `coerce,ANY,data.table`
+
+
+
+## setMethod ===================================================================
+#' @rdname coerce
+#' @export
+setMethod(
+    f = "as.DataFrame",
+    signature = signature("List"),
+    definition = `as.DataFrame,List`
+)
+
+#' @rdname coerce
+#' @export
+setMethod(
+    f = "as.DataFrame",
+    signature = signature("list"),
+    definition = `as.DataFrame,list`
+)
+
+#' @rdname coerce
+#' @export
+setMethod(
+    f = "as.data.frame",
+    signature = signature("IRanges"),
+    definition = `as.data.frame,IRanges`
+)
+
+#' @rdname coerce
+#' @export
+setMethod(
+    f = "as.data.frame",
+    signature = signature("Matrix"),
+    definition = `as.data.frame,Matrix`
+)
+
+
+
+## setAs =======================================================================
+## Ensure these are redefined in basejump, for backward compatibility.
 
 #' @rdname coerce
 #' @name coerce,DataFrame,data.table-method
@@ -601,13 +445,37 @@ setAs(
     def = `coerce,DataFrame,data.table`
 )
 
+#' @rdname coerce
+#' @name coerce,DataFrame,tbl_df-method
+setAs(
+    from = "DataFrame",
+    to = "tbl_df",
+    def = `coerce,DataFrame,tbl_df`
+)
 
+#' @rdname coerce
+#' @name coerce,GenomicRanges,data.table-method
+setAs(
+    from = "GenomicRanges",
+    to = "data.table",
+    def = `coerce,GenomicRanges,data.table`
+)
 
-## Updated 2020-01-19.
-`coerce,IRanges,data.table` <-  # nolint
-    `coerce,ANY,data.table`
+#' @rdname coerce
+#' @name coerce,GenomicRanges,tbl_df-method
+setAs(
+    from = "GenomicRanges",
+    to = "tbl_df",
+    def = `coerce,GenomicRanges,tbl_df`
+)
 
-
+#' @rdname coerce
+#' @name coerce,IRanges,data.frame-method
+setAs(
+    from = "IRanges",
+    to = "data.frame",
+    def = `coerce,IRanges,data.frame`
+)
 
 #' @rdname coerce
 #' @name coerce,IRanges,data.table-method
@@ -617,18 +485,58 @@ setAs(
     def = `coerce,IRanges,data.table`
 )
 
-
-
-## Updated 2019-07-20.
-`coerce,GenomicRanges,data.table` <-  # nolint
-    `coerce,ANY,data.table`
-
-
+#' @rdname coerce
+#' @name coerce,IRanges,tbl_df-method
+setAs(
+    from = "IRanges",
+    to = "tbl_df",
+    def = `coerce,IRanges,tbl_df`
+)
 
 #' @rdname coerce
-#' @name coerce,GenomicRanges,data.table-method
+#' @name coerce,Matrix,DataFrame-method
 setAs(
-    from = "GenomicRanges",
+    from = "Matrix",
+    to = "DataFrame",
+    def = `coerce,Matrix,DataFrame`
+)
+
+#' @rdname coerce
+#' @name coerce,Matrix,data.frame-method
+setAs(
+    from = "Matrix",
+    to = "data.frame",
+    def = `coerce,Matrix,data.frame`
+)
+
+#' @rdname coerce
+#' @name coerce,data.frame,data.table-method
+setAs(
+    from = "data.frame",
     to = "data.table",
-    def = `coerce,GenomicRanges,data.table`
+    def = `coerce,data.frame,data.table`
+)
+
+#' @rdname coerce
+#' @name coerce,data.frame,tbl_df-method
+setAs(
+    from = "data.frame",
+    to = "tbl_df",
+    def = `coerce,data.frame,tbl_df`
+)
+
+#' @rdname coerce
+#' @name coerce,data.table,DataFrame-method
+setAs(
+    from = "data.table",
+    to = "DataFrame",
+    def = `coerce,data.table,DataFrame`
+)
+
+#' @rdname coerce
+#' @name coerce,tbl_df,DataFrame-method
+setAs(
+    from = "tbl_df",
+    to = "DataFrame",
+    def = `coerce,tbl_df,DataFrame`
 )
