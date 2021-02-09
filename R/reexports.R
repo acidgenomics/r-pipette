a0431af975b135685936a8dbb85c16f7e0d6063a
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-09 08:39:41 -0500

    Update NAMESPACE with additional reexports

--- R/reexports.R
+++ R/reexports.R
@@ -16,6 +16,36 @@ AcidBase::pasteURL
 
 
 
+#' @importFrom GenomicRanges GRanges
+#' @export
+GenomicRanges::GRanges
+
+#' @importFrom GenomicRanges GRangesList
+#' @export
+GenomicRanges::GRangesList
+
+
+
+#' @importFrom IRanges DataFrameList
+#' @export
+IRanges::DataFrameList
+
+#' @importFrom IRanges SplitDataFrameList
+#' @export
+IRanges::SplitDataFrameList
+
+
+
+#' @importFrom Matrix Matrix
+#' @export
+Matrix::Matrix
+
+#' @importFrom Matrix sparseMatrix
+#' @export
+Matrix::sparseMatrix
+
+
+
 #' @importFrom RCurl getURL
 #' @export
 RCurl::getURL
@@ -25,6 +55,18 @@ RCurl::getURL
 #' @importFrom S4Vectors DataFrame
 #' @export
 S4Vectors::DataFrame
+
+#' @importFrom S4Vectors List
+#' @export
+S4Vectors::List
+
+#' @importFrom S4Vectors Rle
+#' @export
+S4Vectors::Rle
+
+#' @importFrom S4Vectors SimpleList
+#' @export
+S4Vectors::SimpleList
 
 
 
@@ -55,3 +97,71 @@ tibble::rownames_to_column
 #' @importFrom tibble tibble
 #' @export
 tibble::tibble
+
+
+
+#' @rdname reexports
+#' @name DataFrame-class
+#' @importClassesFrom S4Vectors DataFrame
+#' @exportClass DataFrame
+NULL
+
+#' @rdname reexports
+#' @name DataFrameList-class
+#' @importClassesFrom IRanges DataFrameList
+#' @exportClass DataFrameList
+NULL
+
+#' @rdname reexports
+#' @name GRanges-class
+#' @importClassesFrom GenomicRanges GRanges
+#' @exportClass GRanges
+NULL
+
+#' @rdname reexports
+#' @name GRangesList-class
+#' @importClassesFrom GenomicRanges GRangesList
+#' @exportClass GRangesList
+NULL
+
+#' @rdname reexports
+#' @name IRanges-class
+#' @importClassesFrom IRanges IRanges
+#' @exportClass IRanges
+NULL
+
+#' @rdname reexports
+#' @name List-class
+#' @importClassesFrom S4Vectors List
+#' @exportClass List
+NULL
+
+#' @rdname reexports
+#' @name Matrix-class
+#' @importClassesFrom Matrix Matrix
+#' @exportClass Matrix
+NULL
+
+#' @rdname reexports
+#' @name Rle-class
+#' @importClassesFrom S4Vectors Rle
+#' @exportClass Rle
+NULL
+
+#' @rdname reexports
+#' @name SimpleList-class
+#' @importClassesFrom S4Vectors SimpleList
+#' @exportClass SimpleList
+NULL
+
+#' @rdname reexports
+#' @name SplitDataFrameList-class
+#' @importClassesFrom IRanges SplitDataFrameList
+#' @exportClass SplitDataFrameList
+NULL
+
+#' @rdname reexports
+#' @name sparseMatrix-class
+#' @importClassesFrom Matrix sparseMatrix
+#' @exportClass sparseMatrix
+NULL
