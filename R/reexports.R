b30fbe588b0c1da0ebf44737c78b64e675350083
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-12 15:41:32 -0500

    Migrate reexports back to AcidGenerics

--- R/reexports.R
+++ R/reexports.R
@@ -1,18 +1,6 @@
-#' @importFrom IRanges CharacterList
-#' @export
-IRanges::CharacterList
-
 #' @importFrom AcidGenerics DataFrame
 #' @export
 AcidGenerics::DataFrame
-
-#' @importFrom IRanges DataFrameList
-#' @export
-IRanges::DataFrameList
-
-#' @importFrom IRanges FactorList
-#' @export
-IRanges::FactorList
 
 #' @importFrom GenomicRanges GRanges
 #' @export
@@ -22,33 +10,9 @@ GenomicRanges::GRanges
 #' @export
 GenomicRanges::GRangesList
 
-#' @importFrom IRanges IRanges
-#' @export
-IRanges::IRanges
-
-#' @importFrom IRanges IntegerList
-#' @export
-IRanges::IntegerList
-
-#' @importFrom IRanges LogicalList
-#' @export
-IRanges::LogicalList
-
 #' @importFrom Matrix Matrix
 #' @export
 Matrix::Matrix
-
-#' @importFrom IRanges NumericList
-#' @export
-IRanges::NumericList
-
-#' @importFrom IRanges RleList
-#' @export
-IRanges::RleList
-
-#' @importFrom IRanges SplitDataFrameList
-#' @export
-IRanges::SplitDataFrameList
 
 #' @importFrom tibble column_to_rownames
 #' @export
@@ -90,10 +54,6 @@ AcidGenerics::`metadata<-`
 #' @export
 AcidBase::pasteURL
 
-#' @importFrom IRanges ranges
-#' @export
-IRanges::ranges
-
 #' @importFrom data.table rbindlist
 #' @export
 data.table::rbindlist
@@ -110,35 +70,7 @@ Matrix::sparseMatrix
 #' @export
 tibble::tibble
 
-#' @importFrom IRanges trim
-#' @export
-IRanges::trim
 
-
-
-#' @rdname reexports
-#' @name AtomicList-class
-#' @importClassesFrom IRanges AtomicList
-#' @exportClass AtomicList
-NULL
-
-#' @rdname reexports
-#' @name CharacterList-class
-#' @importClassesFrom IRanges CharacterList
-#' @exportClass CharacterList
-NULL
-
-#' @rdname reexports
-#' @name DataFrameList-class
-#' @importClassesFrom IRanges DataFrameList
-#' @exportClass DataFrameList
-NULL
-
-#' @rdname reexports
-#' @name FactorList-class
-#' @importClassesFrom IRanges FactorList
-#' @exportClass FactorList
-NULL
 
 #' @rdname reexports
 #' @name GRanges-class
@@ -153,45 +85,9 @@ NULL
 NULL
 
 #' @rdname reexports
-#' @name IRanges-class
-#' @importClassesFrom IRanges IRanges
-#' @exportClass IRanges
-NULL
-
-#' @rdname reexports
-#' @name IntegerList-class
-#' @importClassesFrom IRanges IntegerList
-#' @exportClass IntegerList
-NULL
-
-#' @rdname reexports
-#' @name LogicalList-class
-#' @importClassesFrom IRanges LogicalList
-#' @exportClass LogicalList
-NULL
-
-#' @rdname reexports
 #' @name Matrix-class
 #' @importClassesFrom Matrix Matrix
 #' @exportClass Matrix
-NULL
-
-#' @rdname reexports
-#' @name NumericList-class
-#' @importClassesFrom IRanges NumericList
-#' @exportClass NumericList
-NULL
-
-#' @rdname reexports
-#' @name RleList-class
-#' @importClassesFrom IRanges RleList
-#' @exportClass RleList
-NULL
-
-#' @rdname reexports
-#' @name SplitDataFrameList-class
-#' @importClassesFrom IRanges SplitDataFrameList
-#' @exportClass SplitDataFrameList
 NULL
 
 #' @rdname reexports
