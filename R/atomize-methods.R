a6da79c04e5f789ad933fb0e0c7ffcf6a534e9d7
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-24 12:29:56 -0400

    Improve message and working example

--- R/atomize-methods.R
+++ R/atomize-methods.R
@@ -1,30 +1,41 @@
 #' @name atomize
 #' @inherit AcidGenerics::atomize
 #'
+#' @details
+#' Keep only atomic columns. Complex columns won't write to disk as CSVs or work
+#' with R Markdown functions, in some cases.
+#'
+#' @note Updated 2021-08-24.
+#'
 #' @inheritParams AcidRoxygen::params
 #' @param ... Additional arguments.
 #'
 #' @examples
-#' df <- DataFrame(a = "a", b = list(a = seq_len(3)))
+#' df <- DataFrame(
+#'     "a" = "a",
+#'     "b" = I(list("a" = seq_len(3L)))
+#' )
 #' lapply(df, is.atomic)
 #' x <- atomize(df)
 NULL
 
 
 
-## Keep only atomic columns. Complex columns won't write to disk as CSVs or work
-## with R Markdown functions.
-##
-## Updated 2019-08-11.
+## Updated 2021-08-24.
 `atomize,data.frame` <-  # nolint
     function(object) {
         keep <- vapply(X = object, FUN = is.atomic, FUN.VALUE = logical(1L))
         assert(hasLength(keep))
         drop <- names(keep)[!keep]
         if (hasLength(drop)) {
-            message(sprintf(
-                "Dropping non-atomic columns: %s.",
-                toString(drop, width = 100L)
+            alertInfo(sprintf(
+                "Dropping non-atomic %s: %s.",
+                ngettext(
+                    n = length(drop),
+                    msg1 = "column",
+                    msg2 = "columns",
+                ),
+                toInlineString(drop, n = 10L)
             ))
         }
         object[, keep, drop = FALSE]
