4138253eac097d5a6d19b88f943537aa7e29883c
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-12 13:32:01 -0400

    Simplify getOption calls

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -11,32 +11,6 @@
 
 
 
-## FIXME Only include parameters that are used more than 1x.
-
-#' Shared list of optional default formals
-#'
-#' @note Updated 2021-09-24.
-#' @noRd
-.formalsList <- list(
-    "export.dir" = quote(
-        getOption(x = "acid.export.dir", default = getwd())
-    ),
-    "import.make.names" = quote(
-        getOption(x = "acid.import.make.names", default = syntactic::makeNames)
-    ),
-    "import.metadata" = quote(
-        getOption(x = "acid.import.metadata", default = FALSE)
-    ),
-    "overwrite" = quote(
-        getOption(x = "acid.overwrite", default = TRUE)
-    ),
-    "quiet" = quote(
-        getOption(x = "acid.quiet", default = FALSE)
-    )
-)
-
-
-
 #' R data extension pattern
 #'
 #' @note Updated 2020-10-07.
