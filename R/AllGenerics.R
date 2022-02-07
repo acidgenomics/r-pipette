ec5937c775efa8241c627b26fc8884f93a6bca80
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 15:04:44 -0500

    Don't reexport as_tibble and as.data.table

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,138 +1,83 @@
 #' @rdname atomize
 #' @name atomize
-#' @importFrom AcidGenerics atomize
 #' @usage atomize(object, ...)
 #' @export
 NULL
 
-
-
 #' @rdname coerce
 #' @name as.DataFrame
-#' @importFrom AcidGenerics as.DataFrame
 #' @usage as.DataFrame(x, ...)
 #' @export
 NULL
 
-
-
-#' @rdname coerce
-#' @name as.data.table
-#' @importFrom data.table as.data.table
-#' @usage as.data.table(x, keep.rownames = FALSE, ...)
-#' @export
-NULL
-
-
-
-#' @rdname coerce
-#' @name as_tibble
-#' @importFrom tibble as_tibble
-#' @export
-NULL
-
-
-
 #' @rdname decode
 #' @name decode
-#' @importFrom S4Vectors decode
 #' @usage decode(x, ...)
 #' @export
 NULL
 
-
-
 #' @rdname droplevels
 #' @name droplevels
-#' @importFrom S4Vectors droplevels
 #' @usage droplevels(x, ...)
 #' @export
 NULL
 
-
-
 #' @rdname encode
 #' @name encode
-#' @importFrom AcidGenerics encode
 #' @usage encode(x, ...)
 #' @export
 NULL
 
-
-
 #' @rdname export
 #' @name export
-#' @importFrom BiocIO export
 #' @usage export(object, con, format, ...)
 #' @export
 NULL
 
-
-
 #' @rdname factorize
 #' @name factorize
-#' @importFrom AcidGenerics factorize
 #' @usage factorize(object, ...)
 #' @export
 NULL
 
-
-
 #' @rdname import
 #' @name import
-#' @importFrom BiocIO import
 #' @usage import(con, format, text, ...)
 #' @export
 NULL
 
-
-
 #' @rdname matchRownameColumn
 #' @name matchRownameColumn
-#' @importFrom AcidGenerics matchRownameColumn
 #' @usage matchRownameColumn(object, ...)
 #' @export
 NULL
 
-
-
 #' @rdname metadata2
 #' @name metadata2
-#' @importFrom AcidGenerics metadata2
 #' @usage metadata2(x, which, ...)
 #' @export
 NULL
 
 #' @rdname metadata2
 #' @name metadata2<-
-#' @importFrom AcidGenerics metadata2<-
 #' @usage metadata2(x, which, ...) <- value
 #' @export
 NULL
 
-
-
 #' @rdname removeNA
 #' @name removeNA
-#' @importFrom AcidGenerics removeNA
 #' @usage removeNA(object, ...)
 #' @export
 NULL
 
-
-
 #' @rdname sanitizeNA
 #' @name sanitizeNA
-#' @importFrom AcidGenerics sanitizeNA
 #' @usage sanitizeNA(object, ...)
 #' @export
 NULL
 
-
-
 #' @rdname sanitizePercent
 #' @name sanitizePercent
-#' @importFrom AcidGenerics sanitizePercent
 #' @usage sanitizePercent(object, ...)
 #' @export
 NULL
---
+++ R/imports.R
@@ -0,0 +1,51 @@
+## Classes =====================================================================
+#' @importClassesFrom AcidBase missingOrNULL
+#' @importClassesFrom BiocIO BiocFile
+#' @importClassesFrom GenomicRanges GenomicRanges
+#' @importClassesFrom IRanges IntegerRanges Ranges
+#' @importClassesFrom Matrix Matrix
+#' @importClassesFrom S4Vectors DataFrame List SimpleList
+NULL
+
+
+
+## S3 generics and methods =====================================================
+#' @importFrom data.table as.data.table
+#' @importFrom tibble as_tibble
+NULL
+
+
+
+## S4 generics and methods =====================================================
+#' @importFrom AcidGenerics as.DataFrame atomize encode factorize
+#'   matchRownameColumn metadata2 metadata2<- removeNA sanitizeNA
+#'   sanitizePercent
+#' @importFrom BiocGenerics as.data.frame end start strand width
+#' @importFrom BiocIO export import resource
+#' @importFrom GenomicRanges seqnames
+#' @importFrom S4Vectors DataFrame Rle SimpleList decode droplevels head mcols
+#'   mcols<- metadata metadata<- na.omit tail
+#' @importFrom syntactic makeNames
+NULL
+
+
+
+## Standard functions ==========================================================
+#' @importFrom AcidBase basenameSansExt compress decompress dots download
+#'   fileExt initDir methodFunction pasteURL realpath requireNamespaces
+#'   standardizeCall
+#' @importFrom AcidCLI abort alert alertInfo alertSuccess alertWarning
+#'   toInlineString
+#' @importFrom Matrix readMM writeMM
+#' @importFrom RCurl getURL
+#' @importFrom goalie assert allAreAtomic allAreExisting allAreFiles
+#'   allAreNonExisting allAreURLs allHaveAccess areDisjointSets areSameLength
+#'   areSetEqual bapply compressExtPattern extPattern formalCompress hasColnames
+#'   hasCols hasDimnames hasInternet hasLength hasNames hasNoDuplicates
+#'   hasRownames hasRows hasValidNames hasValidDimnames isAFile isAURL isAny
+#'   isCharacter isFlag isInstalled isInt isMatchingRegex isNonNegative
+#'   isPositive isScalar isString isSubset validNames validate
+#' @importFrom methods as is hasMethod new setGeneric signature validObject
+#' @importFrom stringr str_extract str_match str_subset
+#' @importFrom utils download.file packageName packageVersion
+NULL
