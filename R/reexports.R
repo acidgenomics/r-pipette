90e2a199238cf6383a2cd47d6f8eb9f4c1435599
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-03 08:56:59 -0500

    Update reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,11 +1,19 @@
-#' @importFrom data.table data.table
 #' @export
+#' @importFrom tibble column_to_rownames
+tibble::column_to_rownames
+
+#' @export
+#' @importFrom data.table data.table
 data.table::data.table
 
-#' @importFrom RCurl getURL
 #' @export
+#' @importFrom RCurl getURL
 RCurl::getURL
 
-#' @importFrom tibble tibble
 #' @export
+#' @importFrom tibble rownames_to_column
+tibble::rownames_to_column
+
+#' @export
+#' @importFrom tibble tibble
 tibble::tibble
