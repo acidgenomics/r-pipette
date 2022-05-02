8bd7485b7f617f14eb8f513c0d4f3412fec1bc4f
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-02 15:33:56 -0400

    Reformat spacing

--- R/imports.R
+++ R/imports.R
@@ -13,13 +13,13 @@ NULL
 ## S4 generics and methods =====================================================
 
 #' @importFrom AcidGenerics as.DataFrame atomize droplevels2 encode factorize
-#'   matchRownameColumn metadata2 metadata2<- removeNA sanitizeNA
-#'   sanitizePercent
+#' matchRownameColumn metadata2 metadata2<- removeNA sanitizeNA
+#' sanitizePercent
 #' @importFrom BiocGenerics as.data.frame end start strand width
 #' @importFrom BiocIO export import resource
 #' @importFrom GenomicRanges seqnames
 #' @importFrom S4Vectors DataFrame Rle SimpleList decode droplevels head mcols
-#'   mcols<- metadata metadata<- na.omit tail
+#' mcols<- metadata metadata<- na.omit tail
 #' @importFrom syntactic makeNames
 NULL
 
@@ -28,19 +28,19 @@ NULL
 ## Standard functions ==========================================================
 
 #' @importFrom AcidBase basenameSansExt compress decompress dots download
-#'   fileExt initDir methodFunction pasteURL realpath requireNamespaces
-#'   standardizeCall
+#' fileExt initDir methodFunction pasteURL realpath requireNamespaces
+#' standardizeCall
 #' @importFrom AcidCLI abort alert alertInfo alertSuccess alertWarning
-#'   toInlineString
+#' toInlineString
 #' @importFrom Matrix readMM writeMM
 #' @importFrom RCurl getURL
 #' @importFrom goalie assert allAreAtomic allAreExisting allAreFiles
-#'   allAreNonExisting allAreURLs allHaveAccess areDisjointSets areSameLength
-#'   areSetEqual bapply compressExtPattern extPattern formalCompress hasColnames
-#'   hasCols hasDimnames hasInternet hasLength hasNames hasNoDuplicates
-#'   hasRownames hasRows hasValidNames hasValidDimnames isAFile isAURL isAny
-#'   isCharacter isFlag isInstalled isInt isMatchingRegex isNonNegative
-#'   isPositive isScalar isString isSubset validNames validate
+#' allAreNonExisting allAreURLs allHaveAccess areDisjointSets areSameLength
+#' areSetEqual bapply compressExtPattern extPattern formalCompress hasColnames
+#' hasCols hasDimnames hasInternet hasLength hasNames hasNoDuplicates
+#' hasRownames hasRows hasValidNames hasValidDimnames isAFile isAURL isAny
+#' isCharacter isFlag isInstalled isInt isMatchingRegex isNonNegative
+#' isPositive isScalar isString isSubset validNames validate
 #' @importFrom methods as is hasMethod new setGeneric signature validObject
 #' @importFrom utils download.file packageName packageVersion
 NULL
