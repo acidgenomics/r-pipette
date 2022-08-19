d3d7ed013b1722fa71b0b2118dca7444cc362596
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-19 07:08:06 -0400

    Draft update

--- R/factorize-methods.R
+++ R/factorize-methods.R
@@ -1,9 +1,12 @@
 #' @name factorize
 #' @inherit AcidGenerics::factorize
-#' @note Updated 2021-02-11.
+#' @note Updated 2022-08-17.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @param ... Additional arguments.
+#'
+#' @seealso
+#' - Legacy `stringsAsFactors` approach for `data.frame` import.
 #'
 #' @examples
 #' ## DataFrame ====
@@ -16,14 +19,33 @@ NULL
 
 
 
-## Updated 2019-07-19.
+## FIXME Add a setting here (or default?) to only apply as.factor coerction
+## to columns with repeated values...
+
+## FIXME In the unit test, check that non-repeated values do not get factorized.
+
+
+
+## Updated 2022-08-17.
 `factorize,DataFrame` <- # nolint
     function(object) {
         class <- class(object)[[1L]]
         out <- lapply(
             X = object,
             FUN = function(x) {
+                ## FIXME Early return if there are no repeated values
+                ## Use anyDuplicated to check for this and early return.
                 droplevels(as.factor(x))
+
+                ## FIXME Better method that we should consider.
+                ## Which metadata columns should be factorized?
+                apply(
+                    X = colData,
+                    FUN = function(x) {
+                        anyDuplicated(na.omit(x)) > 0L
+                    },
+                    MARGIN = 2L
+                )
             }
         )
         out <- as(out, Class = class)
