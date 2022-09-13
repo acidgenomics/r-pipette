d22989d59c86473c32f4c6604f157221b999aafb
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-13 12:48:41 -0400

    Simplify export methods

--- R/export-methods.R
+++ R/export-methods.R
@@ -107,6 +107,24 @@ NULL
 
 
 
+## Updated 2022-09-13.
+.defaultExt <- function(object) {
+    if (is.character(object)) {
+        key <- "character"
+    }
+    if (is(object, "Matrix")) {
+        key <- "Matrix"
+    } else {
+        key <- "delim"
+    }
+    choices <- .exportFormatChoices
+    ext <- choices[[key]][[1L]]
+    assert(isString(ext))
+    ext
+}
+
+
+
 ## Updated 2021-10-21.
 .exportFormatChoices <- list(
     "Matrix" = c(
@@ -136,6 +154,57 @@ NULL
         "tsv.zip"
     )
 )
+
+
+
+## Updated 2022-09-13.
+`export,ANY,NSE` <-
+    function(object,
+             con,
+             format,
+             dir = getOption(
+                 x = "acid.export.dir",
+                 default = getwd()
+             ),
+             ...) {
+        dots <- list(...)
+        if (isSubset("file", names(dots))) {
+            abort(sprintf(
+                "Use {.arg %s} instead of {.arg %s}.",
+                "con", "file"
+            ))
+        }
+        if (isSubset("ext", names(dots))) {
+            abort(sprintf(
+                "Use {.arg %s} instead of {.arg %s}.",
+                "format", "ext"
+            ))
+        }
+        if (missing(con)) {
+            con <- NULL
+        }
+        if (missing(format)) {
+            format <- NULL
+        }
+        assert(
+            is.null(con),
+            is.null(format),
+            isString(dir)
+        )
+        call <- standardizeCall()
+        sym <- call[["object"]]
+        assert(is.symbol(sym), msg = .symError)
+        name <- as.character(sym)
+        ext <- .defaultExt(object)
+        con <- file.path(dir, paste0(name, ".", ext))
+        args <- list(
+            "object" = object,
+            "con" = con,
+            "format" = format
+        )
+        args <- append(x = args, values = dots)
+        do.call(what = export, args = args)
+    }
 
 
 
@@ -711,4 +780,33 @@ setMethod(
         format = "missing"
     ),
     definition = `export,matrix`
+)
+
+#' @rdname export
+#' @export
+setMethod(
+    f = "export",
+    signature = signature(
+        object = "ANY",
+        con = "missing",
+        format = "missing"
+    ),
+    definition = `export,ANY,NSE`
+)
+
+#' @rdname export
+#' @export
+setMethod(
+    f = "export",
+    signature = signature(
+        object = "ANY",
+        con = "missing",
+        format = "character"
+    ),
+    definition = function(object, con, format) {
+        abort(sprintf(
+            "Use {.arg %s} instead of {.arg %s}.",
+            "con", "format"
+        ))
+    }
 )
