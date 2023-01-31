8928fae0d1e2360723e606d26142d00348064f68
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-31 13:59:50 -0500

    Ensure logicals return unmodified

--- tests/testthat/test-factorize.R
+++ tests/testthat/test-factorize.R
@@ -4,16 +4,16 @@ test_that("DataFrame", {
         "b" = c("a", "a", "b", "b"),
         "c" = c(1L, 2L, 3L, 4L),
         "d" = c(1L, 2L, 1L, 2L),
+        "e" = c(TRUE, TRUE, FALSE, FALSE),
         row.names = c("A", "B", "C", "D")
     )
-    object <- factorize(object)
-    print(object)
     object <- factorize(object)
     expected <- DataFrame(
         "a" = c("a", "b", "c", "d"),
         "b" = as.factor(c("a", "a", "b", "b")),
         "c" = c(1L, 2L, 3L, 4L),
         "d" = as.factor(c(1L, 2L, 1L, 2L)),
+        "e" = c(TRUE, TRUE, FALSE, FALSE),
         row.names = c("A", "B", "C", "D")
     )
     expect_identical(object, expected)
