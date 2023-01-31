066c62aa1c501e59ed5d9dec2203d2bbeefec5b1
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-31 13:59:50 -0500

    Ensure logicals return unmodified

--- R/factorize-methods.R
+++ R/factorize-methods.R
@@ -9,12 +9,13 @@
 #' - Legacy `stringsAsFactors` approach for `data.frame` import.
 #'
 #' @examples
-## DataFrame ====
+#' ## DataFrame ====
 #' object <- S4Vectors::DataFrame(
 #'     "a" = c("a", "b", "c", "d"),
 #'     "b" = c("a", "a", "b", "b"),
 #'     "c" = c(1L, 2L, 3L, 4L),
 #'     "d" = c(1L, 2L, 1L, 2L),
+#'     "e" = c(TRUE, TRUE, FALSE, FALSE),
 #'     row.names = c("A", "B", "C", "D")
 #' )
 #' object <- factorize(object)
