51e5fe477820c9b0e3fcaf8a7fcffa8f3b573328
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-22 15:22:48 -0500

    Rework coverage

--- tests/testthat/test-as.DataFrame.R
+++ tests/testthat/test-as.DataFrame.R
@@ -1,13 +1,13 @@
 test_that("list", {
-    from <- list(
+    lst <- list(
         "a" = list(c(1L, 2L), c(3L, 4L)),
         "b" = list(NULL, NULL)
     )
-    to <- as.DataFrame(from)
-    expect_s4_class(to, "DataFrame")
-    expect_identical(dim(to), c(2L, 2L))
-    expect_identical(
-        object = vapply(X = to, FUN = class, FUN.VALUE = character(1L)),
-        expected = c("a" = "list", "b" = "list")
+    object <- as.DataFrame(lst)
+    expected <- new(
+        Class = "DFrame",
+        listData = lst,
+        nrows = length(lst[[1L]])
     )
+    expect_identical(object, expected)
 })
