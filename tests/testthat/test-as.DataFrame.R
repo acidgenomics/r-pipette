d0296c845d03521e45483c150fa0fcf15241be1f
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-22 15:26:20 -0500

    Improve coercion

--- tests/testthat/test-as.DataFrame.R
+++ tests/testthat/test-as.DataFrame.R
@@ -1,4 +1,8 @@
 test_that("list", {
+    expect_identical(
+        object = as.DataFrame(list()),
+        expected = DataFrame()
+    )
     lst <- list(
         "a" = list(c(1L, 2L), c(3L, 4L)),
         "b" = list(NULL, NULL)
@@ -7,6 +11,17 @@ test_that("list", {
     expected <- new(
         Class = "DFrame",
         listData = lst,
+        nrows = length(lst[[1L]])
+    )
+    expect_identical(object, expected)
+    lst <- list(character(), character())
+    object <- as.DataFrame(lst)
+    expected <- new(
+        Class = "DFrame",
+        listData = list(
+            "X1" = character(),
+            "X2" = character()
+        ),
         nrows = length(lst[[1L]])
     )
     expect_identical(object, expected)
