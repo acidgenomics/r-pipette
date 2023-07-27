22f4d6b634d27bfc233edf2a823d1ec2393632b3
Michael Steinbaugh <mike@steinbaugh.com>
2023-07-27 15:25:42 -0400

    Add coverage for Ensembl REST API

--- tests/testthat/test-getJSON.R
+++ tests/testthat/test-getJSON.R
@@ -1,5 +1,25 @@
 test_that("UCSC Genome Browser API", {
-    url <- "https://api.genome.ucsc.edu/list/ucscGenomes"
+    url <- pasteURL(
+        "api.genome.ucsc.edu",
+        "list",
+        "ucscGenomes",
+        protocol = "https"
+    )
+    json <- getJSON(url)
+    expect_type(json, "list")
+})
+
+test_that("Ensembl REST API", {
+    url <- pasteURL(
+        "rest.ensembl.org",
+        "info",
+        "assembly",
+        paste0(
+            "Homo sapiens",
+            "?", "content-type=application/json"
+        ),
+        protocol = "https"
+    )
     json <- getJSON(url)
     expect_type(json, "list")
 })
