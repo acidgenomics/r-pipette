43958f5b2145a196409a7db757909b257296f5a1
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-25 13:53:04 -0400

    Disabling rest.ensembl.org checks to avoid build check hangs

--- tests/testthat/test-getJSON.R
+++ tests/testthat/test-getJSON.R
@@ -10,18 +10,21 @@ test_that("UCSC Genome Browser API", {
     expect_type(json, "list")
 })
 
-test_that("Ensembl REST API", {
-    url <- pasteURL(
-        "rest.ensembl.org",
-        "info",
-        "assembly",
-        paste0(
-            "Homo sapiens",
-            "?", "content-type=application/json"
-        ),
-        protocol = "https"
-    )
-    skip_if_not_true(isAnExistingURL(url))
-    json <- getJSON(url)
-    expect_type(json, "list")
-})
+## The Ensembl REST API server is prone to connection issues, so disabling this
+## at the moment to avoid build checks hanging.
+
+## > test_that("Ensembl REST API", {
+## >     url <- pasteURL(
+## >         "rest.ensembl.org",
+## >         "info",
+## >         "assembly",
+## >         paste0(
+## >             "Homo sapiens",
+## >             "?", "content-type=application/json"
+## >         ),
+## >         protocol = "https"
+## >     )
+## >     skip_if_not_true(isAnExistingURL(url))
+## >     json <- getJSON(url)
+## >     expect_type(json, "list")
+## > })
