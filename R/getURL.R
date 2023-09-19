70b35b20a376aa96aebf02c47275ba3d3ab5eae5
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-19 12:23:04 -0400

    Export our own simpler getURL variant

---
+++ R/getURL.R
@@ -0,0 +1,34 @@
+#' Download and import a URL
+#'
+#' @export
+#' @note Updated 2023-09-19.
+#'
+#' @return `character`.
+#' Source code lines.
+#'
+#' @examples
+#' url <- "https://ftp.ncbi.nlm.nih.gov/genomes/"
+#' if (goalie::isAnExistingURL(url)) {
+#'     lines <- getURL(url)
+#'     print(head(lines, n = 7L))
+#' }
+getURL <-
+    function(
+        url,
+        quiet = getOption(x = "acid.quiet", default = FALSE)) {
+        assert(
+            isAnExistingURL(url),
+            isFlag(quiet)
+        )
+        destfile <- tempfile()
+        download.file(
+            url = url,
+            destfile = destfile,
+            quiet = quiet
+        )
+        assert(isAFile(destfile))
+        x <- import(con = destfile, format = "lines", quiet = quiet)
+        assert(is.character(x))
+        unlink(destfile)
+        x
+    }
