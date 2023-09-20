541dec4cfea6434b4336bdd1819c595fb83b500c
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-20 09:46:11 -0400

    Add code coverage

--- tests/testthat/test-unfactorize.R
+++ tests/testthat/test-unfactorize.R
@@ -1,8 +1,8 @@
 test_that("factor", {
     vecs <- list(
-        c(100L, 100L, 101L, 101L),
-        c(100.1, 100, 100.1, 100),
-        c("a", "b", "a", "b")
+        c(100L, 100L, 101L, 101L, NA),
+        c(100.1, 100, 100.1, 100, NA),
+        c("a", "b", "a", "b", NA)
     )
     for (vec in vecs) {
         expect_identical(
@@ -13,7 +13,31 @@ test_that("factor", {
 })
 
 test_that("data.frame", {
+    expect_identical(
+        object = unfactorize(data.frame(
+            "a" = as.factor(c(100L, 100L, 101L, 101L)),
+            "b" = as.factor(c("a", "b", "a", "b")),
+            row.names = c("AAA", "BBB", "CCC", "DDD")
+        )),
+        expected = data.frame(
+            "a" = c(100L, 100L, 101L, 101L),
+            "b" = c("a", "b", "a", "b"),
+            row.names = c("AAA", "BBB", "CCC", "DDD")
+        )
+    )
 })
 
 test_that("DFrame", {
+    expect_identical(
+        object = unfactorize(DataFrame(
+            "a" = as.factor(c(100L, 100L, 101L, 101L)),
+            "b" = as.factor(c("a", "b", "a", "b")),
+            row.names = c("AAA", "BBB", "CCC", "DDD")
+        )),
+        expected = DataFrame(
+            "a" = c(100L, 100L, 101L, 101L),
+            "b" = c("a", "b", "a", "b"),
+            row.names = c("AAA", "BBB", "CCC", "DDD")
+        )
+    )
 })
