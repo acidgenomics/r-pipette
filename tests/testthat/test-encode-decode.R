68faea2b1e6bdb5d2be33a5b86c64c0edadb9f3f
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-20 11:10:17 -0400

    Add coverage for specific column selection

--- tests/testthat/test-encode-decode.R
+++ tests/testthat/test-encode-decode.R
@@ -4,6 +4,13 @@ test_that("DFrame", {
     expect_s4_class(x[[1L]], "Rle")
     y <- decode(x)
     expect_s3_class(y[[1L]], "factor")
+    ## Support specific column selection.
+    y <- decode(x, j = 1L)
+    expect_s3_class(y[[1L]], "factor")
+    expect_s4_class(y[[2L]], "Rle")
+    y <- decode(x, j = colnames(x)[[1L]])
+    expect_s3_class(y[[1L]], "factor")
+    expect_s4_class(y[[2L]], "Rle")
 })
 
 test_that("GRanges", {
