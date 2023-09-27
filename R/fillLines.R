cdc469909042747a23e280476321013550331cbb
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-27 06:30:54 -0400

    Add working examples for CSV and TSV

--- R/fillLines.R
+++ R/fillLines.R
@@ -5,6 +5,9 @@
 #'
 #' @param object `character`.
 #'
+#' @param format `character(1)`.
+#' Delimited input format.
+#'
 #' @return `character`.
 #' Modified character vector, with consistent number of delimited values.
 #'
@@ -13,17 +16,40 @@
 #'
 #' @examples
 #' ## character ====
+#' ## CSV mode.
 #' object <- c(
-#'     "aaa\tbbb",
-#'     "ccc\tddd\teee",
-#'     "fff\tggg\thhh\tiii"
+#'     "\"aaa\",\"bbb\",\"ccc\",\"ddd\"",
+#'     "\"eee\",\"fff\",\"ggg\"",
+#'     "\"hhh\",\"iii\""
 #' )
 #' print(object)
-#' object <- fillLines(object, sep = "\t")
+#' object <- fillLines(object, format = "csv")
 #' print(object)
-fillLines <- function(object, sep = c("\t", ",")) {
-    sep <- match.arg(sep)
-    assert(isMatchingFixed(x = object[[1L]], pattern = sep))
+#' con <- textConnection(object)
+#' object <- import(con, format = "csv")
+#' print(object)
+#' close(con)
+#'
+#' ## TSV mode.
+#' object <- c(
+#'     "aaa\tbbb\tccc\tddd",
+#'     "eee\tfff\tggg",
+#'     "hhh\tiii"
+#' )
+#' print(object)
+#' object <- fillLines(object, format = "tsv")
+#' print(object)
+#' con <- textConnection(object)
+#' object <- import(con, format = "tsv")
+#' print(object)
+#' close(con)
+fillLines <- function(object, format = c("csv", "tsv")) {
+    format <- match.arg(format)
+    sep <- switch(EXPR = format, "csv" = ",", "tsv" = "\t")
+    assert(
+        is.character(object),
+        isMatchingFixed(x = object[[1L]], pattern = sep)
+    )
     spl <- strsplit(x = object, split = sep, fixed = TRUE)
     len <- lengths(spl)
     nMax <- max(len)
