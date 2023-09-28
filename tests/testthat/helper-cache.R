2075f7fa073dc64a38ec1940cf77c390bf54723f
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 10:30:28 -0400

    Fix typo

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -3,7 +3,7 @@ if (isFALSE(goalie::hasInternet())) {
     return(invisible(NULL))
 }
 cacheDir <- file.path(
-    tools::R_user_dir(package = .pkgName, which = cacheDir),
+    tools::R_user_dir(package = .pkgName, which = "cache"),
     "testthat"
 )
 dir.create(cacheDir, showWarnings = FALSE)
